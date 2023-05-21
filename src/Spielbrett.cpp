d1e82b4d1f2121fc134c3766decc2e699fefbac1
luke <s05lbaum@uni-bonn.de>
2023-05-21 23:53:41 +0200

    Print fkt gibt alles richtig wieder Reihenfole wird diesmal beachtet

--- src/Spielbrett.cpp
+++ src/Spielbrett.cpp
@@ -17,13 +17,25 @@ namespace vierGewinnt
 
         vector spaltenName = matrixBoard[0];
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 7; i++)
             spaltenName.push_back("0" + to_string(i + 1));
+
         matrixBoard[0] = spaltenName;
-        matrixBoard[2].push_back(ringOne);
+
+        // Probe: wird vom nutzer gemacht!
+        matrixBoard[1].push_back(ringOne);
+        matrixBoard[1].push_back(ringOne);
+        matrixBoard[1].push_back(ringOne);
+
         matrixBoard[2].push_back(ringTwo);
-        matrixBoard[4].push_back(ringOne);
-        matrixBoard[4].push_back(ringOne);
+
+        matrixBoard[3].push_back(ringOne);
+        matrixBoard[3].push_back(ringOne);
+        matrixBoard[3].push_back(ringOne);
+
+        matrixBoard[7].push_back(ringTwo);
+        matrixBoard[7].push_back(ringOne);
+        matrixBoard[7].push_back(ringTwo);
 
         // for (int i = 1; i < matrixBoard.size(); i++)
         //     for (int y = 0; y < 6; y++)
@@ -33,40 +45,28 @@ namespace vierGewinnt
     string Spielbrett::print()
     {
         string returnString;
+        vector<string> returnStringV;
 
         for (string d : matrixBoard[0])
-
-            returnString = returnString + d + " ";
-
-        returnString = returnString + "\n";
+            returnString = returnString + (d + " ");
 
         int d = 0;
-
-        while (d < 7)
+        while (d < 6)
         {
-            for (int i = 1; i < matrixBoard.size(); i++)
+            for (int i = matrixBoard.size() - 1; i >= 1; i--)
             {
                 if (matrixBoard[i].size() > d)
-                    returnString = returnString + matrixBoard[i][d];
+                    returnStringV.push_back(matrixBoard[i][d]);
                 else
-                    returnString = returnString + "-- ";
+                    returnStringV.push_back("-- ");
             }
-            returnString = returnString + "\n";
+            returnStringV.push_back("\n");
             d++;
         }
 
-        /*
-        for (vector<std::string> p : matrixBoard)
-        {
-            for (int d = 0; d < 6; d++)
-            {
-                if (p[d] == ringOne || p[d] == ringTwo)
-                    returnString = returnString + (d + " ");
-            }
-            returnString = returnString + "\n";
-        }
-        */
-
+        for (int i = returnStringV.size() - 1; i >= 0; i--)
+            returnString = returnString + returnStringV[i];
+    
         return returnString;
     }
 
