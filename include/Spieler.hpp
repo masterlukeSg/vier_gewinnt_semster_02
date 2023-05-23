6ab56f573feaf006de21f3513fe2b465a60b20dc
Aicha <s28aerra@uni-bonn.de>
2023-05-23 14:02:53 +0200

    Spieler hpp erstellt

---
+++ include/Spieler.hpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+namespace vierGewinnt {
+
+class Spieler
+{
+private:
+std::string name;
+
+public:
+Spieler(std::string name);
+
+std::string getName();
+
+
+};
+} 
---
+++ src/Spieler.cpp
@@ -0,0 +1,8 @@
+#include <../include/Spieler.hpp>
+#include <iostream>
+#include <string>
+
+namespace vierGewinnt{
+
+    
+}
