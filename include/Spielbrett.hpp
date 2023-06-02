1edfc79acc31a30e139fbd8eb8aa24ca5f4d7598
luke <s05lbaum@uni-bonn.de>
2023-06-02 23:57:47 +0200

    Doxygenkommentare hinzugefuget

--- include/Spielbrett.hpp
+++ include/Spielbrett.hpp
@@ -12,6 +12,7 @@ namespace vierGewinnt
     class Spielbrett
     {
     private:
+    
         int matrix, position;
         std::string ring, unentschieden;
         std::vector<std::string> spaltenName, eins, zwei, drei, vier, fuenf, sechs, sieben;
@@ -20,14 +21,44 @@ namespace vierGewinnt
          * @brief Breite: 7, Höhe: 6
          */
         std::vector<std::vector<std::string>> matrixBoard{spaltenName, eins, zwei, drei, vier, fuenf, sechs, sieben};
+        /**
+         * @brief Überprüft ob man den Ring an der Position platzieren darf
+         * 
+         * @param Position 
+         * @return true
+         * @return false
+         */
         bool legalMove(int Position);
         std::tuple<int, int> getGesetzterRingPosition();
 
     public:
+        /**
+         * @brief Erstellt neues Spielbrettobjekt und fügt die Spaltennamen hinzu
+         */
         Spielbrett();
+        /**
+         * @brief Überprüft ob ein Spieler 4 Ringe horizontal,vertikal oder diagonal platziert hat
+         * @return Gewinner des Spiels 
+         */
         std::string whoIsWinning();
+        /**
+         * @brief Setzt Ring auf Namen des aktuellen Spieler
+         * 
+         * @param player 
+         */
         void setPlayer(Spieler player);
+        /**
+         * @brief Übergibt dem legalMove fkt die Position und setzt es danach an die gewünschte Position
+         * 
+         * @param Position 
+         * @return Position erlaubt (true) oder nicht (false)
+         */
         bool setRing(int Position);
+        /**
+         * @brief Gibt das Spielbrett wieder
+         * 
+         * @return Spielbrett
+         */
         std::string print();
     };
 }
