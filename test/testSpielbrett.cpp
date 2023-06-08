37e76fcc6fe7997bb84213cf21a675e7ac09b614
luke <s05lbaum@uni-bonn.de>
2023-06-08 21:58:58 +0200

    Google Test für Spielbrett

--- test/testSpielbrett.cpp
+++ test/testSpielbrett.cpp
@@ -1,7 +1,11 @@
-/*
+
 #include <gtest/gtest.h>
+#include <vector>
+#include <iostream>
 #include "../src/Spieler.cpp"
 #include "../src/Spielbrett.cpp"
+
+// cmake -S . -B build && cmake --build build
 
 using namespace vierGewinnt;
 
@@ -12,4 +16,14 @@ TEST(Spielbrett, setRing)
     board.setPlayer(player);
     EXPECT_EQ(board.setRing(4), true);
 }
-*/
+
+TEST(Spielbrett, whoIsWinning)
+{
+    Spielbrett board = Spielbrett();
+    Spieler player = Spieler("Lukas", "LU");
+    board.setPlayer(player);
+    std::vector<int> list = {1, 2, 3, 4};
+    for (int f : list)
+        board.setRing(f);
+    EXPECT_EQ(board.whoIsWinning(), "LU") << "Gewinner wurde nicht richtig erkannt";
+}
